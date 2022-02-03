87a98738131abf9f6ddb92583f228f5f4d58c05a
vorph <klimczak.mathieu@pm.me>
2022-02-03 21:48:43 +0100

    test avec glob

--- perso/src/main.rs
+++ perso/src/main.rs
@@ -1,47 +1,62 @@
 use error_chain::error_chain;
-use glob::glob;
+use glob::{glob_with, MatchOptions};
 use std::{fs, io};
 
 error_chain! {
     foreign_links {
         Io(std::io::Error);
         SystemTimeError(std::time::SystemTimeError);
+        Glob(glob::GlobError);
+        Pattern(glob::PatternError);
     }
 }
 
 fn main() -> Result<()> {
     let current_dir = give_path_dir();
+    let extension = "/**/*.png";
 
     println!(
-        "Entries modified in the last 24 hours in {:?}:",
-        current_dir
+        "Entries found in {:?} with the following extension {:?}:",
+        current_dir, extension
     );
 
-    // let num_files = fs::read_dir(&current_dir).len();
+    let options = MatchOptions {
+        case_sensitive: false,
+        ..Default::default()
+    };
 
-    for entry in fs::read_dir(&current_dir)? {
-        let entry = entry?;
-        let path = entry.path();
+    let files_wanted = add_extension(current_dir, extension);
 
-        let metadata = fs::metadata(&path)?;
-        println!(
-            "is read only: {:?}, size: {:?} bytes, filename: {:?}",
-            metadata.permissions().readonly(),
-            metadata.len(),
-            path.file_name().ok_or("No filename")?
-        );
-        //let last_modified = metadata.modified()?.elapsed()?.as_secs();
-
-        // if last_modified < 24 * 3600 && metadata.is_file() {
-        //     println!(
-        //         "Last modified: {:?} seconds, is read only: {:?}, size: {:?} bytes, filename: {:?}",
-        //         last_modified,
-        //         metadata.permissions().readonly(),
-        //         metadata.len(),
-        //         path.file_name().ok_or("No filename")?
-        //     );
-        // }
+    for entry in glob_with(&files_wanted, options).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => println!("{:?}", path.display()),
+            Err(e) => println!("{:?}", e),
+        }
     }
+
+    // for entry in fs::read_dir(&current_dir)? {
+    //     let entry = entry?;
+    //     let path = entry.path();
+
+    //     let metadata = fs::metadata(&path)?;
+    //     println!(
+    //         "is read only: {:?}, size: {:?} bytes, filename: {:?}",
+    //         metadata.permissions().readonly(),
+    //         metadata.len(),
+    //         path.file_name().ok_or("No filename")?
+    //     );
+    //let last_modified = metadata.modified()?.elapsed()?.as_secs();
+
+    // if last_modified < 24 * 3600 && metadata.is_file() {
+    //     println!(
+    //         "Last modified: {:?} seconds, is read only: {:?}, size: {:?} bytes, filename: {:?}",
+    //         last_modified,
+    //         metadata.permissions().readonly(),
+    //         metadata.len(),
+    //         path.file_name().ok_or("No filename")?
+    //     );
+    // }
+    // }
 
     Ok(())
 }
@@ -55,12 +70,15 @@ fn give_path_dir() -> String {
         .read_line(&mut current_dir)
         .expect("Nothing to read !");
 
-    current_dir = match current_dir.trim().parse() {
-        Ok(path) => path,
-        Err(_) => {
-            panic!("Not a valid path !");
-        }
-    }; // the trim() function return a slice of a string, so you have to parse it again to get a string
+    current_dir = current_dir.trim().parse().expect("No path found !"); // the trim() function return a slice of a string, so you have to parse it again to get a string
+
+    return current_dir;
+}
+
+fn add_extension(mut current_dir: String, extension: &str) -> String {
+    current_dir.push_str(extension);
+
+    println!("{:?}", current_dir);
 
     return current_dir;
 }
@@ -71,22 +89,15 @@ fn give_path_dir() -> String {
 // use error_chain::error_chain;
 // use glob::{glob_with, MatchOptions};
 
-// error_chain! {
-//     foreign_links {
-//         Glob(glob::GlobError);
-//         Pattern(glob::PatternError);
-//     }
-// }
-
 // fn main() -> Result<()> {
-//     let options = MatchOptions {
-//         case_sensitive: false,
-//         ..Default::default()
-//     };
+// let options = MatchOptions {
+//     case_sensitive: false,
+//     ..Default::default()
+// };
 
-//     for entry in glob_with("/media/img_[0-9]*.png", options)? {
-//         println!("{}", entry?.display());
-//     }
+// for entry in glob_with("/media/img_[0-9]*.png", options)? {
+//     println!("{}", entry?.display());
+// }
 
 //     Ok(())
 // }
